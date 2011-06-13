0626d03b370ab684eb8a1b8c2f6f5d9e7b827a1c
Simon Tokumine <simon@japancentre.com>
2011-06-13 05:02:52 +0100

    Remove cruft

--- test/unit/app.test.js
+++
@@ -1,8 +0,0 @@
-require('../helper');
-
-var _      = require('underscore')
-  , assert = require('assert');
-
-exports['test String#length'] = function(){
-    assert.equal(6, 'foobar'.length);
-};
