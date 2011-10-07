ca0dd963f277cd1879751d326cbc62656f106b70
Simon Tokumine <simon@japancentre.com>
2011-10-07 17:46:00 +0100

    remove

--- config/deploy.rb
+++
@@ -1,8 +0,0 @@
-set :spinner_user, nil #weird hack
-set :stages, %w(staging production)
-require 'capistrano/ext/multistage'
-require 'capistrano/ext/monitor'
-#require "bundler/capistrano"
-#require 'capistrano/gitflow'
-ssh_options[:keys] = [File.join("config", "deploy", "ssh", "id-vizzuality.pem")]
-
