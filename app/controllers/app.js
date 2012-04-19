5f27cb896e21e22f1a46f46f2f97c3861cdf8391
Simon Tokumine <si@tinypla.net>
2012-04-19 11:25:09 -0400

    update close

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -91,7 +91,7 @@ function handleQuery(req, res){
                 if (err) throw err;
 
                 // close connection to psql. be nice.
-                pg.end();
+                //pg.end();
 
                 // configure headers for geojson/CSV
                 res.header("Content-Disposition", getContentDisposition(format));
