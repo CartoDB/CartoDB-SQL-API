dae217cec7e05fae4f29e72295b7df981dc82dd8
Simon Tokumine <si@tinypla.net>
2012-06-01 20:50:09 +0100

    psql units passing

--- test/unit/psql.test.js
+++ test/unit/psql.test.js
@@ -52,7 +52,7 @@ exports['test username returns interpolated db if user set'] = function(){
 //};
 
 exports['test private user can execute SELECTS on db'] = function(){  
-  var pg = new PSQL('simon');
+  var pg = new PSQL('1');
   var sql = "SELECT 1 as test_sum"
   pg.query(sql, function(err, result){
     assert.equal(result.rows[0].test_sum, 1);
@@ -61,7 +61,7 @@ exports['test private user can execute SELECTS on db'] = function(){
 };
 
 exports['test private user can execute CREATE on db'] = function(){  
-  var pg = new PSQL('simon');
+  var pg = new PSQL('1');
   var sql = "DROP TABLE IF EXISTS distributors; CREATE TABLE distributors (id integer, name varchar(40), UNIQUE(name))"
   pg.query(sql, function(err, result){
     assert.isNull(err);
@@ -70,7 +70,7 @@ exports['test private user can execute CREATE on db'] = function(){
 };
 
 exports['test private user can execute INSERT on db'] = function(){  
-  var pg = new PSQL('simon');
+  var pg = new PSQL('1');
   var sql = "DROP TABLE IF EXISTS distributors1; CREATE TABLE distributors1 (id integer, name varchar(40), UNIQUE(name))"
   pg.query(sql, function(err, result){
     sql = "INSERT INTO distributors1 (id, name) VALUES (1, 'fish')"
@@ -82,12 +82,12 @@ exports['test private user can execute INSERT on db'] = function(){
 };
 
 exports['test publicuser can execute SELECT on enabled tables'] = function(){  
-  var pg = new PSQL("simon");    
+  var pg = new PSQL("1");
   var sql = "DROP TABLE IF EXISTS distributors2; CREATE TABLE distributors2 (id integer, name varchar(40), UNIQUE(name)); GRANT SELECT ON distributors2 TO publicuser;"  
   pg.query(sql, function(err, result){
     pg.end();
     
-    pg = new PSQL(null, 'cartodb_test_user_simon_db');    
+    pg = new PSQL(null, 'cartodb_test_user_1_db');
     pg.query("SELECT count(*) FROM distributors2", function(err, result){
       assert.equal(result.rows[0].count, 0);
       pg.end();      
@@ -96,12 +96,12 @@ exports['test publicuser can execute SELECT on enabled tables'] = function(){
 }
 
 exports['test publicuser cannot execute INSERT on db'] = function(){
-  var pg = new PSQL("simon");     
+  var pg = new PSQL("1");
   var sql = "DROP TABLE IF EXISTS distributors3; CREATE TABLE distributors3 (id integer, name varchar(40), UNIQUE(name)); GRANT SELECT ON distributors3 TO publicuser;"
   pg.query(sql, function(err, result){    
     pg.end();
     
-    pg = new PSQL(null, 'cartodb_test_user_simon_db'); //anonymous user
+    pg = new PSQL(null, 'cartodb_test_user_1_db'); //anonymous user
     pg.query("INSERT INTO distributors3 (id, name) VALUES (1, 'fishy')", function(err, result){
       assert.eql(err.message, 'permission denied for relation distributors3')
       pg.end();      
