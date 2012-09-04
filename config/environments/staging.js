d5a6002e2e9395ab5c76a5c81577878eb3bd15e6
Sandro Santilli <strk@keybit.net>
2012-09-04 11:16:11 +0200

    Fix db_base_name and db_user setting for staging environment

--- config/environments/staging.js
+++ config/environments/staging.js
@@ -1,8 +1,8 @@
 module.exports.node_port    = 8080;
 module.exports.node_host    = '127.0.0.1';
 module.exports.environment  = 'staging';
-module.exports.db_base_name = 'cartodb_user_<%= user_id %>_db';
-module.exports.db_user      = 'cartodb_user_<%= user_id %>';
+module.exports.db_base_name = 'cartodb_staging_user_<%= user_id %>_db';
+module.exports.db_user      = 'cartodb_staging_user_<%= user_id %>';
 module.exports.db_host      = 'localhost';
 module.exports.db_port      = '6432';
 module.exports.redis_host   = '127.0.0.1';
