6a2c0e9727c6baf4e7bd918f73eed6637d43006c
Sandro Santilli <strk@keybit.net>
2012-09-11 12:22:27 +0200

    Initial support for SVG output (#49)
    This version only dumps the geometries with no identifier.
    Scales geometries to fit in a 1024x768 pixels area, using
    a circle radius of 5 pixels, a stroke-width of 1 pixel and
    no fill.  Supports trimming number of decimals.
    Adds a viewBox tag to fit the drawing to the output device window.
    
    Includes an automated testcase.

--- app/controllers/app.js
+++ app/controllers/app.js
@@ -50,6 +50,10 @@ function handleQuery(req, res) {
     var offset    = parseInt(req.query.page);
     var format    = req.query.format;
     var dp        = req.query.dp; // decimal point digits (defaults to 6)
+    var gn        = "the_geom"; // TODO: read from configuration file 
+    var svg_width  = 1024.0;
+    var svg_height = 768.0;
+
 
     // sanitize and apply defaults to input
     dp        = (dp       === "" || _.isUndefined(dp))       ? '6'  : dp;
@@ -129,6 +133,27 @@ function handleQuery(req, res) {
                 // TODO: refactor formats to external object
                 if (format === 'geojson'){
                     sql = ['SELECT *, ST_AsGeoJSON(the_geom,',dp,') as the_geom FROM (', sql, ') as foo'].join("");
+                } else if (format === 'svg'){
+                    var svg_ratio = svg_width/svg_height;
+                    sql = 'WITH source AS ( ' + sql + '), extent AS ( ' 
+                        + ' SELECT ST_Extent(' + gn + ') AS e FROM source '
+                        + '), extent_info AS ( SELECT e, '
+                        + 'st_xmin(e) as ex0, st_ymax(e) as ey0, '
+                        + 'st_xmax(e)-st_xmin(e) as ew, '
+                        + 'st_ymax(e)-st_ymin(e) as eh FROM extent )'
+                        + ', trans AS ( SELECT CASE WHEN '
+                        + 'eh = 0 THEN ' + svg_width
+                        + '/ COALESCE(NULLIF(ew,0),' + svg_width +') WHEN '
+                        + svg_ratio + ' <= (ew / eh) THEN ('
+                        + svg_width  + '/ew ) ELSE ('
+                        + svg_height + '/eh ) END as s '
+                        + ', ex0 as x0, ey0 as y0 FROM extent_info ) '
+                        + 'SELECT st_TransScale(e, -x0, -y0, s, s)::box2d as '
+                        + gn + '_box, ST_Dimension(' + gn + ') as ' + gn
+                        + '_dimension, ST_AsSVG(ST_TransScale(' + gn + ', '
+                        + '-x0, -y0, s, s), 0, ' + dp + ') as ' + gn
+                        //+ ', ex0, ey0, ew, eh, s ' // DEBUG ONLY
+                        + ' FROM trans, extent_info, source';
                 }
 
                 pg.query(sql, this);
@@ -156,6 +181,8 @@ function handleQuery(req, res) {
                 // TODO: refactor formats to external object
                 if (format === 'geojson'){
                     toGeoJSON(result, res, this);
+                } else if (format === 'svg'){
+                    toSVG(result.rows, gn, this);
                 } else if (format === 'csv'){
                     toCSV(result, res, this);
                 } else {
@@ -221,6 +248,98 @@ function toGeoJSON(data, res, callback){
     }
 }
 
+function toSVG(rows, gn, callback){
+
+    var radius = 5; // in pixels (based on svg_width and svg_height)
+    var stroke_width = 1; // in pixels (based on svg_width and svg_height)
+    var stroke_color = 'black';
+    // fill settings affect polygons and points (circles)
+    var fill_opacity = 0.5; // 0.0 is fully transparent, 1.0 is fully opaque
+                            // unused if fill_color='none'
+    var fill_color = 'none'; // affects polygons and circles
+
+    var bbox; // will be computed during the results scan
+    var polys = [];
+    var lines = [];
+    var points = [];
+    _.each(rows, function(ele){
+        var g = ele[gn];
+        if ( ! g ) return; // null or empty
+        var gdims = ele[gn + '_dimension'];
+
+        // TODO: add an identifier, if any of "cartodb_id", "oid", "id", "gid" are found
+        // TODO: add "class" attribute to help with styling ?
+        if ( gdims == '0' ) {
+          points.push('<circle r="[RADIUS]" ' + g + ' />');
+        } else if ( gdims == '1' ) {
+          // Avoid filling closed linestrings
+          var linetag = '<path ';
+          if ( fill_color != 'none' ) linetag += 'fill="none" '
+          linetag += 'd="' + g + '" />';
+          lines.push(linetag);
+        } else if ( gdims == '2' ) {
+          polys.push('<path d="' + g + '" />');
+        }
+
+        if ( ! bbox ) { 
+          // Parse layer extent: "BOX(x y, X Y)"
+          // NOTE: the name of the extent field is
+          //       determined by the same code adding the
+          //       ST_AsSVG call (in queryResult)
+          //
+          bbox = ele[gn + '_box'];
+          bbox = bbox.match(/BOX\(([^ ]*) ([^ ,]*),([^ ]*) ([^)]*)\)/);
+          bbox = {
+            xmin: parseFloat(bbox[1]),
+            ymin: parseFloat(bbox[2]), 
+            xmax: parseFloat(bbox[3]),
+            ymax: parseFloat(bbox[4]) 
+           };
+        }
+    });
+
+    // Set point radius
+    for (var i=0; i<points.length; ++i) {
+      points[i] = points[i].replace('[RADIUS]', radius);
+    }
+
+    var header_tags = [
+        '<?xml version="1.0" standalone="no"?>',
+        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+    ];
+
+    var root_tag = '<svg ';
+    if ( bbox ) {
+      // expand box by "radius" + "stroke-width"
+      // TODO: use a Box2d class for these ops
+      var growby = radius+stroke_width;
+      bbox.xmin -= growby;
+      bbox.ymin -= growby;
+      bbox.xmax += growby;
+      bbox.ymax += growby;
+      bbox.width = bbox.xmax - bbox.xmin;
+      bbox.height = bbox.ymax - bbox.ymin;
+      root_tag += 'viewBox="' + bbox.xmin + ' ' + (-bbox.ymax) + ' '
+               + bbox.width + ' ' + bbox.height + '" '; 
+    }
+    root_tag += 'style="fill-opacity:' + fill_opacity
+              + '; stroke:' + stroke_color
+              + '; stroke-width:' + stroke_width
+              + '; fill:' + fill_color
+              + '" ';
+    root_tag += 'xmlns="http://www.w3.org/2000/svg" version="1.1">';
+
+    header_tags.push(root_tag);
+
+    // Render points on top of lines and lines on top of polys
+    var out = header_tags.concat(polys, lines, points);
+
+    out.push('</svg>');
+
+    // return payload
+    callback(null, out.join("\n"));
+}
+
 function toCSV(data, res, callback){
     try{
         // pull out keys for column headers
@@ -241,8 +360,11 @@ function getContentDisposition(format){
     if (format === 'geojson'){
         ext = 'geojson';
     }
-    if (format === 'csv'){
+    else if (format === 'csv'){
         ext = 'csv';
+    }
+    else if (format === 'svg'){
+        ext = 'svg';
     }
     var time = new Date().toUTCString();
     return 'inline; filename=cartodb-query.' + ext + '; modification-date="' + time + '";';
@@ -252,6 +374,9 @@ function getContentType(format){
     var type = "application/json; charset=utf-8";
     if (format === 'csv'){
         type = "text/csv; charset=utf-8";
+    }
+    else if (format === 'svg'){
+        type = "image/svg+xml; charset=utf-8";
     }
     return type;
 }
