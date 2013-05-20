CartoDB/CartoDB-SQL-API

commit fcf95755b6c412e58350cd1ebe3cf1a241d2d2cb

Fixed problem identifying OAuth request protocol
The protocol of a OAuth request wasn't being identified correctly. It
always considered that the request protocol was https.

app/models/oauth.js
@@ -66,7 +66,7 @@ var oAuth = function(){
   me.verifyRequest = function(req, callback){
     var that = this;
     //TODO: review this
-    var http = arguments['2'];
+    var httpProto = arguments['2'];
     var passed_tokens;
     var ohash;
     var signature;
@@ -99,7 +99,16 @@ var oAuth = function(){
 
         var method = req.method;
         var host   = req.headers.host;
-        var path   = http ? 'http://' + host + req.route.path : 'https://' + host + req.route.path;
+
+        if(!httpProto || (httpProto != 'http' && httpProto != 'https')) {
+          var msg = "Unknown HTTP protocol.";
+          err = new Error(msg);
+          err.http_status = 500;
+          callback(err);
+          return;
+        }
+        
+        var path   = httpProto + '://' + host + req.route.path;
         that.splitParams(req.query);
 
         // remove signature from passed_tokens

app/controllers/app.js
@@ -109,6 +109,7 @@ function handleQuery(req, res) {
     var gn        = "the_geom"; // TODO: read from configuration file
     var user_id;
     var tableCacheItem;
+    var requestProtocol = req.protocol;
 
     try {
 
@@ -180,7 +181,7 @@ function handleQuery(req, res) {
                 if(api_key) {
                     ApiKeyAuth.verifyRequest(req, this);
                 } else {
-                    oAuth.verifyRequest(req, this);
+                    oAuth.verifyRequest(req, this, requestProtocol);
                 }
             },
             function queryExplain(err, data){

commit e5ad8f0760ff09cbce8763773e538faea293060b

Fix test broken after oAuth interface change introduced in fcf95755
Also improve error message on miscall

app/models/oauth.js
@@ -101,7 +101,7 @@ var oAuth = function(){
         var host   = req.headers.host;
 
         if(!httpProto || (httpProto != 'http' && httpProto != 'https')) {
-          var msg = "Unknown HTTP protocol.";
+          var msg = "Unknown HTTP protocol " + httpProto + ".";
           err = new Error(msg);
           err.http_status = 500;
           callback(err);

test/unit/oauth.test.js
@@ -76,7 +76,7 @@ test('can return user for verified signature', function(done){
         assert.ok(!err, err);
         assert.equal(data, 1);
         done();
-    }, true);
+    }, 'http');
 });
 
 test('returns null user for unverified signatures', function(done){
@@ -89,7 +89,7 @@ test('returns null user for unverified signatures', function(done){
     oAuth.verifyRequest(req, function(err, data){
         assert.equal(data, null);
         done();
-    }, true);
+    }, 'http');
 });
 
 test('returns null user for no oauth', function(done){