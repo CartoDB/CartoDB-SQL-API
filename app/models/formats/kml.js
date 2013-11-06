45d54c47cc94ae938ceb7cd4a54cb343b0f110cd
Sandro Santilli <strk@keybit.net>
2013-11-06 08:06:29 +0100

    Add note about which GDAL version fixes the SRID detection bug

--- app/models/formats/kml.js
+++ app/models/formats/kml.js
@@ -8,12 +8,13 @@ var p = kml.prototype;
 
 p._contentType = "application/kml; charset=utf-8";
 p._fileExtension = "kml";
-// As of GDAL 1.10 SRID detection is bogus, so we use
+// As of GDAL 1.10.1 SRID detection is bogus, so we use
 // our own method. See:
 //  http://trac.osgeo.org/gdal/ticket/5131
 //  http://trac.osgeo.org/gdal/ticket/5287
 //  http://github.com/CartoDB/CartoDB-SQL-API/issues/110
 //  http://github.com/CartoDB/CartoDB-SQL-API/issues/116
+// Bug was fixed in GDAL 1.10.2
 p._needSRS = true;
 
 p.generate = function(options, callback) {
