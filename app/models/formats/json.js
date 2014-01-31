5c22b8ddaae59e5de41b4f25e0a338ba43e4590a
Sandro Santilli <strk@keybit.net>
2014-01-31 15:47:33 +0100

    Drop unused code

--- app/models/formats/json.js
+++ app/models/formats/json.js
@@ -8,38 +8,6 @@ var p = json.prototype;
 
 p._contentType = "application/json; charset=utf-8";
 
-var typeNames = {
-    16: 'boolean', // bool
-    17: 'string', // bytea
-    20: 'number', // int8
-    21: 'number', // int2
-    23: 'number', // int4
-    25: 'string', // text
-    26: 'number', // oid
-   114: 'object', // JSON
-   700: 'number', // float4
-   701: 'number', // float8
-  1000: 'boolean[]', // _bool
-  1015: 'string[]', // _varchar
-  1042: 'string', // bpchar
-  1043: 'string', // varchar
-  1005: 'number[]', // _int2
-  1007: 'number[]', // _int4
-  1014: 'string[]', // _bpchar
-  1016: 'number[]', // _int8
-  1021: 'number[]', // _float4
-  1022: 'number[]', // _float8
-  1008: 'string[]', // _regproc
-  1009: 'string[]', // _text
-  1114: 'date', // timestamp without timezone
-  1182: 'date', // date
-  1184: 'date', // timestamp
-  1186: 'string', // interval
-  1231: 'number[]', // _numeric
-  1700: 'number', // numeric
-
-};
-
 p.formatResultFields = function(flds) {
   var nfields = {};
   for (var i=0; i<flds.length; ++i) {
