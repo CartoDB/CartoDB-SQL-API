742904c6e6d14c25326d5a1beb3ca7cf6296ecb2
Raul Ochoa <rochoaf@gmail.com>
2014-09-24 11:05:16 +0200

    Adds debug info about $PGUSER var

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,14 +4,13 @@
 export TZ='Europe/Rome'
 export PGAPPNAME='cartodb_sqlapi_tester'
 
-export
-
 # In case PGUSER env variable does not exist we attempt to use `postgres`
 if test x"${PGUSER}" = x; then
-  PGUSER=postgres
+    echo "PGUSER not found"
+    PGUSER=postgres
+else
+    echo "PGUSER found = ${PGUSER}"
 fi
-
-export
 
 OPT_CREATE_PGSQL=yes # create/prepare the postgresql test database
 OPT_CREATE_REDIS=yes # create/prepare the redis test databases
