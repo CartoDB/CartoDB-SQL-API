6874ef823aa9879de51b808f91c0af24a49ff82f
Luis Bosque <luisico@gmail.com>
2015-03-25 18:39:45 +0100

    jshint fixes

--- test/unit/health_check.js
+++ test/unit/health_check.js
@@ -77,7 +77,7 @@ suite('health checks', function() {
     test('error if disabled file exists', function(done) {
       var fs = require('fs');
 
-      readFileFn = fs.readFile
+      var readFileFn = fs.readFile;
       fs.readFile = function(filename, callback) {
         callback(null, "Maintenance");
       }
@@ -85,22 +85,22 @@ suite('health checks', function() {
         assert.equal(err.message, "Maintenance");
         assert.equal(err.http_status, 503);
         done();
+        fs.readFile = readFileFn;
       });
-      fs.readFile = readFileFn;
     });
 
     test('not err if disabled file does not exists', function(done) {
       var fs = require('fs');
-
-      readFileFn = fs.readFile
+      
+      var readFileFn = fs.readFile;
       fs.readFile = function(filename, callback) {
-        throw "ENOENT"
+        callback(new Error("ENOENT"), null);
       }
       healthCheck.check('fake', 'select 1', function(err, result) {
         assert.equal(err, null);
         done();
+        fs.readFile = readFileFn;
       });
-      fs.readFile = readFileFn;
     });
 
     function mockGetAllUserDBParams(func) {
