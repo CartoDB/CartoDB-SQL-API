ed070e30403c74d2edf71b3405856741a30dbd6b
dgaubert <danielgarciaaubert@gmail.com>
2016-04-01 15:03:32 +0200

    Used SCAN instead of KEYS for looking for queues when Batch API starts

--- batch/job_subscriber.js
+++ batch/job_subscriber.js
@@ -10,24 +10,50 @@ function JobSubscriber(redis, metadataBackend) {
 
 JobSubscriber.prototype.subscribe = function (onMessage) {
     var self = this;
-    var redisParams = [ this.redisPrefix + '*' ];
 
-    self.metadataBackend.redisCmd(self.db, 'KEYS', redisParams , function (err, queues) {
+    self._lookForQueues(onMessage, function (err) {
         if (err) {
             console.error(err);
-        }
-
-        if (queues) {
-            queues.forEach(function (queue) {
-                var host = queue.substr(queue.lastIndexOf(':') + 1);
-                onMessage(self.channel, host);
-            });
         }
 
         self.client.subscribe(self.channel);
         self.client.on('message', onMessage);
     });
 };
+
+JobSubscriber.prototype._lookForQueues = function (onMessage, callback) {
+    var pattern = this.redisPrefix + '*';
+
+    scanQueues(this.metadataBackend, this.db, ['0'], pattern, this.channel, onMessage, callback);
+};
+
+function scanQueues(metadataBackend, db, cursor, pattern, channel, onMessage, callback) {
+    var redisParams = [cursor[0], 'MATCH', pattern];
+
+    metadataBackend.redisCmd(db, 'SCAN', redisParams, function (err, currentCursor) {
+        if (err) {
+            return callback(err);
+        }
+
+        // checks if iteration has ended
+        if (currentCursor[0] === '0') {
+            return callback(null);
+        }
+
+        var queues = currentCursor[1];
+
+        if (!queues) {
+            return callback(null);
+        }
+
+        queues.forEach(function (queue) {
+            var host = queue.substr(queue.lastIndexOf(':') + 1);
+            onMessage(channel, host);
+        });
+
+        scanQueues(metadataBackend, db, currentCursor, pattern, channel, onMessage, callback);
+    });
+}
 
 JobSubscriber.prototype.unsubscribe = function () {
     this.client.unsubscribe(this.channel);
