61bbdd6db48f689b4bac458b81a2809906e2decf
Daniel García Aubert <dgaubert@cartodb.com>
2016-04-05 18:52:12 +0200

    Removed unused code

--- batch/job_status.js
+++ batch/job_status.js
@@ -10,9 +10,3 @@ var JOB_STATUS_ENUM = {
 };
 
 module.exports = JOB_STATUS_ENUM;
-module.exports.keys = function keys() {
-    return Object.keys(JOB_STATUS_ENUM);
-};
-module.exports.values = function values() {
-    return Object.values(JOB_STATUS_ENUM);
-};
