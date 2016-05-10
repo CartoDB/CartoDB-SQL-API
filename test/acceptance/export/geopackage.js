7b9911416b2e97f54e357dca2765ffe289a866f1
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-05-10 18:01:50 +0200

    geopackage export test #291

--- test/acceptance/export/geopackage.js
+++ test/acceptance/export/geopackage.js
@@ -3,6 +3,7 @@ require('../../helper');
 var app = require(global.settings.app_root + '/app/app')();
 var assert = require('../../support/assert');
 var sqlite = require('sqlite3');
+var fs      = require('fs');
 
 describe('geopackage query', function(){
     // Default name, cartodb-query, fails because of the hyphen.
@@ -32,22 +33,46 @@ describe('geopackage query', function(){
         assert.response(app, {
             url: '/api/v1/sql?q=SELECT%20*%20FROM%20untitle_table_4%20LIMIT%201&format=geopackage&filename=' + table_name,
             headers: {host: 'vizzuality.cartodb.com'},
+            encoding: 'binary',
             method: 'GET'
         },{ }, function(res) {
-            var db = new sqlite.Database(':memory:', res.body);
-            var schemaQuery = "SELECT name, sql FROM sqlite_master WHERE type='table' ORDER BY name";
-            var qr = db.get(schemaQuery, function(err){
-                assert.equal(err, null);
-                done();
-            });
-            assert.notEqual(qr, undefined);
+            var tmpfile = '/tmp/a_geopackage_file.gpkg';
+            try {
+              fs.writeFileSync(tmpfile, res.body, 'binary');
+            } catch(err) {
+              return done(err);
+            }
 
-            var gpkgQuery = "SELECT table_name FROM gpkg_contents";
-            var gqr = db.get(gpkgQuery, function(err){
-                assert.equal(err, null);
-                done();
+            var db = new sqlite.Database(tmpfile, function(err) {
+              if(!!err) {
+                return done(err);
+              }
+
+              db.serialize(function() {
+                var schemaQuery = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY name";
+                var sqr = db.get(schemaQuery, function(err, row) {
+                  assert.equal(err, null);
+                  assert.equal(row.name, table_name);
+                });
+                assert.notEqual(sqr, undefined);
+
+                var gpkgQuery = "SELECT table_name FROM gpkg_contents";
+                var gqr = db.get(gpkgQuery, function(err, row) {
+                  assert.equal(row.table_name, table_name);
+                  assert.equal(err, null);
+                });
+                assert.notEqual(gqr, undefined);
+
+                var dataQuery = "SELECT * FROM " + table_name + " order by cartodb_id";
+                var dqr = db.get(dataQuery, function(err, row) {
+                  assert.equal(err, null);
+                  assert.equal(row.cartodb_id, 1);
+                  assert.equal(row.name, 'Hawai');
+                  done();
+                });
+                assert.notEqual(dqr, undefined);
+              });
             });
-            assert.notEqual(gqr, undefined);
         });
     });
 
