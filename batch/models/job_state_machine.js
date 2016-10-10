deb1ccf876380df8aab8f21f9ee7b7467b641115
Raul Ochoa <rochoaf@gmail.com>
2016-10-10 12:09:13 +0200

    DRY job final statuses

--- batch/models/job_state_machine.js
+++ batch/models/job_state_machine.js
@@ -1,24 +1,17 @@
 'use strict';
 
 var assert = require('assert');
-var jobStatus = require('../job_status');
-var finalStatus = [
-    jobStatus.CANCELLED,
-    jobStatus.DONE,
-    jobStatus.FAILED,
-    jobStatus.UNKNOWN
-];
-
+var JobStatus = require('../job_status');
 var validStatusTransitions = [
-    [jobStatus.PENDING, jobStatus.RUNNING],
-    [jobStatus.PENDING, jobStatus.CANCELLED],
-    [jobStatus.PENDING, jobStatus.UNKNOWN],
-    [jobStatus.PENDING, jobStatus.SKIPPED],
-    [jobStatus.RUNNING, jobStatus.DONE],
-    [jobStatus.RUNNING, jobStatus.FAILED],
-    [jobStatus.RUNNING, jobStatus.CANCELLED],
-    [jobStatus.RUNNING, jobStatus.PENDING],
-    [jobStatus.RUNNING, jobStatus.UNKNOWN]
+    [JobStatus.PENDING, JobStatus.RUNNING],
+    [JobStatus.PENDING, JobStatus.CANCELLED],
+    [JobStatus.PENDING, JobStatus.UNKNOWN],
+    [JobStatus.PENDING, JobStatus.SKIPPED],
+    [JobStatus.RUNNING, JobStatus.DONE],
+    [JobStatus.RUNNING, JobStatus.FAILED],
+    [JobStatus.RUNNING, JobStatus.CANCELLED],
+    [JobStatus.RUNNING, JobStatus.PENDING],
+    [JobStatus.RUNNING, JobStatus.UNKNOWN]
 ];
 
 function JobStateMachine () {
@@ -42,5 +35,5 @@ JobStateMachine.prototype.isValidTransition = function (initialStatus, finalStat
 };
 
 JobStateMachine.prototype.isFinalStatus = function (status) {
-    return finalStatus.indexOf(status) !== -1;
+    return JobStatus.isFinal(status);
 };
