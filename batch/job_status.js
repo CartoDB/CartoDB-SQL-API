d84d49e36bcb239ffe9854733822dabba16b93de
Daniel García Aubert <dgaubert@cartodb.com>
2016-11-23 12:40:19 +0100

    Add status SKIPPED as final one

--- batch/job_status.js
+++ batch/job_status.js
@@ -16,6 +16,7 @@ var finalStatus = [
     JOB_STATUS_ENUM.CANCELLED,
     JOB_STATUS_ENUM.DONE,
     JOB_STATUS_ENUM.FAILED,
+    JOB_STATUS_ENUM.SKIPPED,
     JOB_STATUS_ENUM.UNKNOWN
 ];
 module.exports.isFinal = function(status) {
