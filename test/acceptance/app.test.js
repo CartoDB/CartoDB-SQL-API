cf00287b7338e138441c2ef9e5da7eff7e7175ea
Raul Ochoa <rochoaf@gmail.com>
2016-12-09 11:10:08 +0100

    COPY statements suite

--- test/acceptance/app.test.js
+++ test/acceptance/app.test.js
@@ -473,47 +473,6 @@ it('CREATE TABLE with GET and auth', function(done){
     });
 });
 
-// Test effects of COPY
-// See https://github.com/Vizzuality/cartodb-management/issues/1502
-it('COPY TABLE with GET and auth', function(done){
-    assert.response(server, {
-        url: "/api/v1/sql?" + querystring.stringify({
-          q: 'COPY test_table FROM stdin;',
-          api_key: 1234
-        }),
-        headers: {host: 'vizzuality.cartodb.com'},
-        method: 'GET'
-    },{}, function(err, res) {
-      // We expect a problem, actually
-      assert.equal(res.statusCode, 400, res.statusCode + ': ' + res.body);
-      assert.deepEqual(res.headers['content-type'], 'application/json; charset=utf-8');
-      assert.deepEqual(res.headers['content-disposition'], 'inline');
-      assert.deepEqual(JSON.parse(res.body), {"error":["COPY from stdin failed: No source stream defined"]});
-      done();
-    });
-});
-
-it('COPY TABLE with GET and auth', function(done){
-    assert.response(server, {
-        url: "/api/v1/sql?" + querystring.stringify({
-          q: "COPY test_table to '/tmp/x';",
-          api_key: 1234
-        }),
-        headers: {host: 'vizzuality.cartodb.com'},
-        method: 'GET'
-    },{}, function(err, res) {
-      // We expect a problem, actually
-      assert.equal(res.statusCode, 400, res.statusCode + ': ' + res.body);
-      assert.deepEqual(res.headers['content-type'], 'application/json; charset=utf-8');
-      assert.deepEqual(res.headers['content-disposition'], 'inline');
-      assert.deepEqual(JSON.parse(res.body), {
-          error: ["must be superuser to COPY to or from a file"],
-          hint: "Anyone can COPY to stdout or from stdin. psql's \\copy command also works for anyone."
-      });
-      done();
-    });
-});
-
 it('ALTER TABLE with GET and auth', function(done){
     assert.response(server, {
         url: "/api/v1/sql?" + querystring.stringify({
