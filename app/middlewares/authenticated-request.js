e04529c5018d9a0dfc4be7e998578df008585d5b
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-02-19 14:20:09 +0100

    Rename middleware

--- app/middlewares/authenticated-request.js
+++ app/middlewares/authenticated-request.js
@@ -3,17 +3,15 @@
 var _ = require('underscore');
 var AuthApi = require('../auth/auth_api');
 
-function authenticatedMiddleware(userDatabaseService, forceToBeAuthenticated = false) {
-    return function middleware(req, res, next) {
+module.exports = function authenticatedRequest (userDatabaseService, forceToBeAuthenticated = false) {
+    return function authenticatedRequestMiddleware (req, res, next) {
         req.profiler.start('sqlapi.job');
         req.profiler.done('init');
 
-        // clone so don't modify req.params or req.body so oauth is not broken
         const params = _.extend({}, res.locals, req.query, req.body);
-
         const { user } = res.locals;
+        const authApi = new AuthApi(req, res, params);
 
-        var authApi = new AuthApi(req, res, params);
         userDatabaseService.getConnectionParams(authApi, user, function (err, dbParams, authDbParams, userLimits) {
             req.profiler.done('setDBAuth');
 
@@ -32,6 +30,4 @@ function authenticatedMiddleware(userDatabaseService, forceToBeAuthenticated = f
             next();
         });
     };
-}
-
-module.exports = authenticatedMiddleware;
+};
