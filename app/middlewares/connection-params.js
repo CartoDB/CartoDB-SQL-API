868dc798b2f7f84ad1d1c5f19506f59a5ca6b6ea
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-02-22 15:59:13 +0100

    Cosmetic line break

--- app/middlewares/connection-params.js
+++ app/middlewares/connection-params.js
@@ -2,7 +2,8 @@ module.exports = function connectionParams (userDatabaseService) {
     return function connectionParamsMiddleware (req, res, next) {
         const { user, api_key: apikeyToken, authenticated } = res.locals;
 
-        userDatabaseService.getConnectionParams(user, apikeyToken, authenticated, function (err, userDbParams, authDbParams) {
+        userDatabaseService.getConnectionParams(user, apikeyToken, authenticated,
+            function (err, userDbParams, authDbParams) {
             if (req.profiler) {
                 req.profiler.done('getConnectionParams');
             }
