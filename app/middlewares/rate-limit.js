a3dfd2767b9d94ef596aaa1a428748dc89c8dfb2
Simon Martín <oleurud@gmail.com>
2018-03-01 12:19:28 +0100

    rate limit middleware

---
+++ app/middlewares/rate-limit.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const RATE_LIMIT_ENDPOINTS_GROUPS = {
+    QUERY: 'query',
+    QUERY_FORMAT: 'query_format',
+    JOB_CREATE: 'job_create',
+    JOB_GET: 'job_get',
+    JOB_DELETE: 'job_delete'
+};
+
+
+function rateLimitFn(userLimits, endpointGroup = null) {
+    return function rateLimitMiddleware(req, res, next) {
+        if (!global.settings.ratelimits.rateLimitsEnabled) {
+            return next();
+        }
+
+        const user = res.locals.user;
+
+        if (!endpointGroup || !isRateLimitEnabledByEndpoint(endpointGroup)) {
+            return next();
+        }
+
+        userLimits.getRateLimit(user, endpointGroup, function(err, rateLimit) {
+            if (err) {
+                return next(err);
+            }
+    
+            if (!rateLimit) {
+                return next();
+            }
+    
+            const [isBlocked, limit, remaining, retry, reset] = rateLimit;
+    
+            res.set({
+                'X-Rate-Limit-Limit': limit,
+                'X-Rate-Limit-Remaining': remaining,
+                'X-Rate-Limit-Retry-After': retry,
+                'X-Rate-Limit-Reset': reset
+            });
+    
+            if (isBlocked) {
+                const err = new Error('You are over the limits.');
+                err.http_status = 429;
+                return next(err);
+            }
+    
+            return next();
+        });
+    };
+}
+
+
+function isRateLimitEnabledByEndpoint(endpointGroup) {
+    return global.settings.ratelimits.endpoints[endpointGroup] === true;
+}
+
+
+module.exports = rateLimitFn;
+module.exports.RATE_LIMIT_ENDPOINTS_GROUPS = RATE_LIMIT_ENDPOINTS_GROUPS;
