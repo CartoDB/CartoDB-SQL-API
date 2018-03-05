0dd7c1f8789c22e135623deef9e355968dc711ce
Simon Martín <oleurud@gmail.com>
2018-03-05 16:29:27 +0100

    ls

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -113,8 +113,10 @@ TESTS=$@
 if test x"$OPT_CREATE_REDIS" = xyes; then
   echo "Starting redis on port ${REDIS_PORT}"
   BASEDIR2=$(pwd)
-  echo "${BASEDIR2}"
-  echo "port ${REDIS_PORT}" | redis-server - --loadmodule ./libredis_cell.so > ${BASEDIR}/test.log &
+  echo "${CURRENTPATH}"
+  LISTDIR=$(ls -la)
+  echo "${LISTDIR}"
+  echo "port ${REDIS_PORT}" | redis-server - --loadmodule ${CURRENTPATH}/libredis_cell.so > ${BASEDIR}/test.log &
   PID_REDIS=$!
   echo ${PID_REDIS} > ${BASEDIR}/redis.pid
 fi
