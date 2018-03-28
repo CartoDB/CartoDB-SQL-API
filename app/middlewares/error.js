16ea727fc3f8bbb46525dab3b68e058fdb08e09e
Simon Martín <oleurud@gmail.com>
2018-03-28 12:58:10 +0200

    move ErrorHandler logic from error middleware

--- app/middlewares/error.js
+++ app/middlewares/error.js
@@ -1,9 +1,8 @@
 var ErrorHandler = require('../services/error_handler');
-var pgErrorCodes = require('../postgresql/error_codes');
 
 module.exports = function errorMiddleware() {
     return function error(err, req, res, next) {
-        let errorHandler = getErrorHandler(err);
+        let errorHandler = ErrorHandler.getErrorHandler(err);
 
         var msg = errorHandler.getResponse();
 
@@ -84,40 +83,4 @@ function stringifyForLogs(object) {
     });
 
     return JSON.stringify(object);
-}
-
-function getErrorHandler (err) {
-    if (isTimeoutError(err)) {
-        return createTimeoutError();
-    } else {
-        return createGenericError(err);
-    }
-}
-
-function isTimeoutError(err) {
-    return err.message && (
-        err.message.indexOf('statement timeout') > -1 ||
-        err.message.indexOf('RuntimeError: Execution of function interrupted by signal') > -1
-    );
-}
-
-function createTimeoutError() {
-    return new ErrorHandler(
-        'You are over platform\'s limits. Please contact us to know more details',
-        'limit',
-        'datasource',
-        undefined,
-        429
-    );
-}
-
-function createGenericError(err) {
-    return new ErrorHandler(
-        err.message,
-        err.context, 
-        err.detail, 
-        err.hint, 
-        err.http_status, 
-        pgErrorCodes.codeToCondition[err.code] || err.name
-    );
 }
--- app/services/error_handler.js
+++ app/services/error_handler.js
@@ -1,3 +1,5 @@
+const pgErrorCodes = require('../postgresql/error_codes');
+
 class ErrorHandler extends Error {
     constructor(message, context, detail, hint, http_status = 400, name = null) {
         super(message);
@@ -27,6 +29,42 @@ class ErrorHandler extends Error {
         }
     
         return http_status;
+    }
+
+    static getErrorHandler (err) {
+        if (this.isTimeoutError(err)) {
+            return this.createTimeoutError();
+        } else {
+            return this.createGenericError(err);
+        }
+    }
+    
+    static isTimeoutError(err) {
+        return err.message && (
+            err.message.indexOf('statement timeout') > -1 ||
+            err.message.indexOf('RuntimeError: Execution of function interrupted by signal') > -1
+        );
+    }
+    
+    static createTimeoutError() {
+        return new ErrorHandler(
+            'You are over platform\'s limits. Please contact us to know more details',
+            'limit',
+            'datasource',
+            undefined,
+            429
+        );
+    }
+    
+    static createGenericError(err) {
+        return new ErrorHandler(
+            err.message,
+            err.context, 
+            err.detail, 
+            err.hint, 
+            err.http_status, 
+            pgErrorCodes.codeToCondition[err.code] || err.name
+        );
     }
 }
 
