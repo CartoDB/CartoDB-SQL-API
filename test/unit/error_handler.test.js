8e5ac72411c6a69b2ea40d08a7965ffd833c33ad
Simon Martín <oleurud@gmail.com>
2018-03-28 12:10:48 +0200

    simplify error_handler tests

--- test/unit/error_handler.test.js
+++ test/unit/error_handler.test.js
@@ -3,7 +3,37 @@
 var assert = require('assert');
 var errorMiddleware = require('../../app/middlewares/error');
 
-describe('error-handler', function() {
+const req = { query: { callback: true } };
+
+const getRes = () => {
+    return {
+        headers: {},
+        set (key, value) {
+            this.headers[key] = value;
+        },
+        header (key, value) {
+            this.set(key, value);
+        },
+        statusCode: 0,
+        status (status) {
+            this.statusCode = status;
+        },
+        json () {},
+        jsonp () {}
+    }
+};
+
+const getErrorHeader = (context, detail, hint, message) => {
+    return {
+        context,
+        detail,
+        hint,
+        statusCode: 400,
+        message
+    }
+};
+
+describe.only('error-handler', function() {
     it('should return a header with errors', function (done) {
 
         let error = new Error('error test');
@@ -11,29 +41,14 @@ describe('error-handler', function() {
         error.hint = 'test hint';
         error.context = 'test context';
 
-        const req = {};
-        const res = {
-            headers: {},
-            set (key, value) {
-                this.headers[key] = value;
-            },
-            header (key, value) {
-                this.set(key, value);
-            },
-            statusCode: 0,
-            status (status) {
-                this.statusCode = status;
-            },
-            json () {}
-        };
+        const errorHeader = getErrorHeader(
+            error.context,
+            error.detail,
+            error.hint,
+            error.message
+        );
 
-        const errorHeader = {
-            detail: error.detail,
-            hint: error.hint,
-            context: error.context,
-            statusCode: 400,
-            message: error.message
-        };
+        const res = getRes();
 
         errorMiddleware()(error, req, res, function next () {
             assert.ok(res.headers['X-SQLAPI-Errors'].length > 0);
@@ -52,31 +67,14 @@ describe('error-handler', function() {
         error.hint = 'test hint';
         error.context = 'test context';
 
-        const req = {
-            query: { callback: true }
-        };
-        const res = {
-            headers: {},
-            set (key, value) {
-                this.headers[key] = value;
-            },
-            header (key, value) {
-                this.set(key, value);
-            },
-            statusCode: 0,
-            status (status) {
-                this.statusCode = status;
-            },
-            jsonp () {}
-        };
+        const errorHeader = getErrorHeader(
+            error.context,
+            error.detail,
+            error.hint,
+            error.message
+        );
 
-        const errorHeader = {
-            detail: error.detail,
-            hint: error.hint,
-            context: error.context,
-            statusCode: 400,
-            message: error.message
-        };
+        const res = getRes();        
 
         errorMiddleware()(error, req, res, function next () {
             assert.ok(res.headers['X-SQLAPI-Errors'].length > 0);
@@ -98,32 +96,15 @@ describe('error-handler', function() {
         error.hint = badString;
         error.context = badString;
 
-        const req = {
-            query: { callback: true }
-        };
-        const res = {
-            headers: {},
-            set (key, value) {
-                this.headers[key] = value;
-            },
-            header (key, value) {
-                this.set(key, value);
-            },
-            statusCode: 0,
-            status (status) {
-                this.statusCode = status;
-            },
-            jsonp () {}
-        };
+        const errorHeader = getErrorHeader(
+            escapedString,
+            escapedString,
+            escapedString,
+            escapedString
+        );
 
-        const errorHeader = {
-            detail: escapedString,
-            hint: escapedString,
-            context: escapedString,
-            statusCode: 400,
-            message: escapedString
-        };
-
+        const res = getRes();
+        
         errorMiddleware()(error, req, res, function () {
             assert.ok(res.headers['X-SQLAPI-Errors'].length > 0);
             assert.deepEqual(
