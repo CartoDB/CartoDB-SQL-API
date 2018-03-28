eefea03b80555ca56b8a8c9b90c56f1bad7ce8ba
Simon Martín <oleurud@gmail.com>
2018-03-28 10:48:25 +0200

    error handler to class

--- app/postgresql/error_handler.js
+++ app/postgresql/error_handler.js
@@ -1,49 +1,52 @@
 var pgErrorCodes = require('./error_codes');
 
-function ErrorHandler(err) {
-    this.err = err;
-
-    if (this.isTimeoutError()) {
-        this.err = new Error('You are over platform\'s limits. Please contact us to know more details');
-        this.err.http_status = 429;
-        this.err.context = 'limit';
-        this.err.detail = 'datasource';
+class ErrorHandler {
+    constructor(err) {
+        this.err = err;
+    
+        if (this.isTimeoutError()) {
+            this.err = new Error('You are over platform\'s limits. Please contact us to know more details');
+            this.err.http_status = 429;
+            this.err.context = 'limit';
+            this.err.detail = 'datasource';
+        }
     }
+
+    getName () {
+        return pgErrorCodes.codeToCondition[this.err.code] || this.err.name;
+    };
+    
+    getMessage () {
+        return this.err.message;
+    };
+    
+    getFields () {
+        return {
+            detail: this.err.detail,
+            hint: this.err.hint,
+            context: this.err.context,
+        };
+    };
+    
+    getStatus () {
+        var statusError = this.err.http_status || 400;
+    
+        var message = this.getMessage();
+    
+        if (message && message.match(/permission denied/)) {
+            statusError = 403;
+        }
+    
+        return statusError;
+    };
+    
+    isTimeoutError () {
+        return this.err.message && (
+            this.err.message.indexOf('statement timeout') > -1 ||
+            this.err.message.indexOf('RuntimeError: Execution of function interrupted by signal') > -1
+        );
+    };
+    
 }
 
 module.exports = ErrorHandler;
-
-ErrorHandler.prototype.getName = function() {
-    return pgErrorCodes.codeToCondition[this.err.code] || this.err.name;
-};
-
-ErrorHandler.prototype.getMessage = function() {
-    return this.err.message;
-};
-
-ErrorHandler.prototype.getFields = function() {
-    return {
-        detail: this.err.detail,
-        hint: this.err.hint,
-        context: this.err.context,
-    };
-};
-
-ErrorHandler.prototype.getStatus = function() {
-    var statusError = this.err.http_status || 400;
-
-    var message = this.getMessage();
-
-    if (message && message.match(/permission denied/)) {
-        statusError = 403;
-    }
-
-    return statusError;
-};
-
-ErrorHandler.prototype.isTimeoutError = function() {
-    return this.err.message && (
-        this.err.message.indexOf('statement timeout') > -1 ||
-        this.err.message.indexOf('RuntimeError: Execution of function interrupted by signal') > -1
-    );
-};
