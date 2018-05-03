af4c5906c85584f988d133ba52cfbfb0fc3d5260
Simon Martín <oleurud@gmail.com>
2018-05-03 18:31:49 +0200

    adding /sql tro the endpoints

--- app/controllers/copy_controller.js
+++ app/controllers/copy_controller.js
@@ -76,8 +76,8 @@ CopyController.prototype.route = function (app) {
         ];
     };
 
-    app.post(`${base_url}/copyfrom`, copyFromMiddlewares(RATE_LIMIT_ENDPOINTS_GROUPS.QUERY));
-    app.get(`${base_url}/copyto`, copyToMiddlewares(RATE_LIMIT_ENDPOINTS_GROUPS.QUERY));
+    app.post(`${base_url}/sql/copyfrom`, copyFromMiddlewares(RATE_LIMIT_ENDPOINTS_GROUPS.QUERY));
+    app.get(`${base_url}/sql/copyto`, copyToMiddlewares(RATE_LIMIT_ENDPOINTS_GROUPS.QUERY));
 };
 
 CopyController.prototype.copyDbParamsToReq = function (req, res, next) {
