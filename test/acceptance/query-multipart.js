8dd1d5babf1ade4a898287b640affa2d043c6dcd
Paul Ramsey <pramsey@cleverelephant.ca>
2018-05-22 15:24:19 -0400

    Try and quiet multipart query

--- test/acceptance/query-multipart.js
+++ test/acceptance/query-multipart.js
@@ -15,7 +15,7 @@ describe('query-multipart', function() {
         },{}, function(err, res) {
             assert.ifError(err);
             const response = JSON.parse(res.body);
-            assert.equal(!!response.time, true);
+            assert.equal(typeof(response.time) !== 'undefined', true);
             assert.strictEqual(response.total_rows, 1);
             assert.deepStrictEqual(response.rows, [{n:2}]);
             done();
