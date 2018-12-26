2eff945c5b02fcba1cf73117b9bb708e2da1829b
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-12-26 18:27:41 +0100

    Don't be able to run docker-test against Node.js 6

--- test/run_tests_docker.sh
+++ test/run_tests_docker.sh
@@ -2,7 +2,7 @@
 
 usage() {
     /etc/init.d/postgresql stop
-    echo "Usage: $0 [nodejs10|nodejs6]"
+    echo "Usage: $0"
     exit 1
 }
 
@@ -17,31 +17,10 @@ echo "$0 $1"
 echo "Node.js version:"
 node -v
 
-# install dependencies
-NODEJS_VERSION=${1-nodejs10}
-
-if [ "$NODEJS_VERSION" = "nodejs10" ];
-then
-    echo "npm version on install:"
-    npm -v
-    mv npm-shrinkwrap.json npm-shrinkwrap.json.backup
-    npm ci
-    npm ls
-    mv npm-shrinkwrap.json.backup npm-shrinkwrap.json
-elif [ "$NODEJS_VERSION" = "nodejs6" ];
-then
-    echo "npm version on install:"
-    npm -v
-    mv package-lock.json package-lock.json.backup
-    npm i
-    npm ls
-    mv package-lock.json.backup package-lock.json
-else
-    usage
-fi
+echo "npm version:"
+npm -v
+npm ci
+npm ls
 
 # run tests
-echo "npm version on tests:"
-npm -v
-
 npm test
