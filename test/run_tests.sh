955b72a87c179e3a81704ddabc34fd316c463362
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-01-16 18:13:17 +0100

    Upgrade to mocha 5 to fix vulns

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -148,10 +148,10 @@ echo
 
 if test x"$OPT_COVERAGE" = xyes; then
   echo "Running tests with coverage"
-  ./node_modules/.bin/istanbul cover node_modules/.bin/_mocha -- -u tdd --trace -t 5000 ${TESTS}
+  ./node_modules/.bin/istanbul cover node_modules/.bin/_mocha -- -u tdd --trace -t 5000 --exit ${TESTS}
 else
   echo "Running tests"
-  mocha -u tdd -t 5000 ${TESTS}
+  mocha -u tdd -t 5000 --exit ${TESTS}
 fi
 ret=$?
 
