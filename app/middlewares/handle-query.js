143af82b8bc2905aa0170683a1aad736b4c899b0
Simon Martín <oleurud@gmail.com>
2019-02-26 14:40:41 +0100

    semicolon

--- app/middlewares/handle-query.js
+++ app/middlewares/handle-query.js
@@ -2,7 +2,7 @@
 
 module.exports = function handleQuery () {
     return function handleQueryMiddleware (req, res, next) {
-        res.locals.q = (req.body && req.body.q) || (req.query && req.query.q)
+        res.locals.q = (req.body && req.body.q) || (req.query && req.query.q);
         return next();
     };
 };
