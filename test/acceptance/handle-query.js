9f4adbc3de54b0e47591783ad46da30fc14495ad
Simon Martín <oleurud@gmail.com>
2019-02-26 15:43:14 +0100

    Handle query middleware failing tests

--- test/acceptance/handle-query.js
+++ test/acceptance/handle-query.js
@@ -8,18 +8,40 @@ const qs = require('querystring');
 
 const QUERY = `SELECT 14 as foo`;
 const API_KEY = 1234;
-const BODY_PAYLOAD = {
-    q: QUERY,
-    api_key: API_KEY
-};
 
 describe.only('Handle query middleware', function() {
     ['GET', 'POST'].forEach(method => {
+        it(`${method} without query fails`, function(done) {
+            assert.response(server,
+                {
+                    method,
+                    url: '/api/v1/sql?' + qs.stringify({
+                        api_key: API_KEY
+                    }),
+                    headers: {
+                        host: 'vizzuality.cartodb.com'
+                    }
+                },
+                { statusCode: 400 },
+                function(err, res) {
+                    assert.ok(!err);
+
+                    const response = JSON.parse(res.body);
+                    assert.deepEqual(response, { error: ["You must indicate a sql query"] });
+
+                    done();
+                }
+            );
+        });
+
         it(`${method} query`, function(done) {
             assert.response(server,
                 {
                     method,
-                    url: '/api/v1/sql?' + qs.stringify(BODY_PAYLOAD),
+                    url: '/api/v1/sql?' + qs.stringify({
+                        q: QUERY,
+                        api_key: API_KEY
+                    }),
                     headers: {
                         host: 'vizzuality.cartodb.com'
                     }
