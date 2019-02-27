d5470ac5569c5e4f9272a66f02db24c1a0ef3db2
Simon Martín <oleurud@gmail.com>
2019-02-27 12:50:23 +0100

    testing config parameters usage

--- test/acceptance/log.js
+++ test/acceptance/log.js
@@ -10,7 +10,18 @@ const BatchTestClient = require('../support/batch-test-client');
 const QUERY = `SELECT 14 as foo`;
 const API_KEY = 1234;
 
+const logQueries = global.settings.logQueries;
+const maxQueriesLogLength = global.settings.maxQueriesLogLength;
+
 describe('Log middleware', function() {
+    before(function() {
+        global.settings.logQueries = true;
+    });
+
+    after(function() {
+        global.settings.logQueries = logQueries;
+    });
+
     describe('regular queries endpoint', function() {
         ['GET', 'POST'].forEach(method => {
             it(`${method} without query fails`, function(done) {
@@ -119,6 +130,86 @@ describe('Log middleware', function() {
 
                 return done();
             });
+        });
+    });
+
+    describe('disable queries log', function() {
+        before(function() {
+            global.settings.logQueries = false;
+        });
+
+        after(function() {
+            global.settings.logQueries = true;
+        });
+
+        it(`GET query`, function(done) {
+            assert.response(server,
+                {
+                    method: 'GET',
+                    url: '/api/v1/sql?' + qs.stringify({
+                        q: QUERY,
+                        api_key: API_KEY
+                    }),
+                    headers: {
+                        host: 'vizzuality.cartodb.com'
+                    }
+                },
+                { statusCode: 200 },
+                function(err, res) {
+                    assert.ok(!err);
+
+                    assert.ok(res.headers['x-sqlapi-log']);
+                    const log = JSON.parse(res.headers['x-sqlapi-log']);
+                    assert.deepEqual(log, {
+                        request: {
+                            sql: null
+                        }
+                    });
+
+                    return done();
+                }
+            );
+        });
+    });
+
+    describe('modify queries log length', function() {
+        before(function() {
+            global.settings.maxQueriesLogLength = 2;
+        });
+
+        after(function() {
+            global.settings.maxQueriesLogLength = maxQueriesLogLength;
+        });
+
+        it(`GET query`, function(done) {
+            assert.response(server,
+                {
+                    method: 'GET',
+                    url: '/api/v1/sql?' + qs.stringify({
+                        q: QUERY,
+                        api_key: API_KEY
+                    }),
+                    headers: {
+                        host: 'vizzuality.cartodb.com'
+                    }
+                },
+                { statusCode: 200 },
+                function(err, res) {
+                    assert.ok(!err);
+
+                    assert.ok(res.headers['x-sqlapi-log']);
+                    const log = JSON.parse(res.headers['x-sqlapi-log']);
+                    assert.deepEqual(log, {
+                        request: {
+                            sql: {
+                                simple: QUERY.substring(0, global.settings.maxQueriesLogLength)
+                            }
+                        }
+                    });
+
+                    return done();
+                }
+            );
         });
     });
 });
