9e25cc9501d9889bee0812ac2f36205b469c882f
Simon Martín <oleurud@gmail.com>
2019-02-27 10:07:56 +0100

    accept multiple values types in queries log

--- app/middlewares/log.js
+++ app/middlewares/log.js
@@ -17,5 +17,24 @@ module.exports = function log() {
 };
 
 function prepareSQL(sql) {
-    return (sql && sql.substring(0, MAX_SQL_LENGTH)) || null;
+    if (!sql) {
+        return null;
+    }
+
+    if (typeof sql === 'string') {
+        return {
+            simple: sql.substring(0, MAX_SQL_LENGTH)
+        }
+    }
+
+    if (Array.isArray(sql)) {
+        return {
+            multiple: sql.map(q => q.substring(0, MAX_SQL_LENGTH))
+        }
+    }
+
+    return {
+        other: sql
+    }
+
 }
