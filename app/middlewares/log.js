a97c25e9146a41786dca7c0d43d4662203d21041
Simon Martín <oleurud@gmail.com>
2019-02-28 11:48:51 +0100

    adding TYPES in middleware

--- app/middlewares/log.js
+++ app/middlewares/log.js
@@ -2,13 +2,18 @@
 
 const { stringifyForLogs } = require('../utils/logs');
 
-module.exports = function log() {
+const TYPES = {
+    QUERY: 'query',
+    JOB: 'job'
+};
+
+module.exports = function log(sqlType = TYPES.QUERY) {
     return function logMiddleware(req, res, next) {
         const MAX_SQL_LENGTH = (global.settings.logQueries && global.settings.maxQueriesLogLength) || 1024;
 
         const logObj = {
             request: {
-                sql: prepareSQL(res.locals.sql, MAX_SQL_LENGTH)
+                sql: prepareSQL(res.locals.sql, sqlType, MAX_SQL_LENGTH)
             }
         };
 
@@ -18,24 +23,30 @@ module.exports = function log() {
     };
 };
 
-function prepareSQL(sql, MAX_SQL_LENGTH) {
+module.exports.TYPES = TYPES;
+
+function prepareSQL(sql, sqlType, MAX_SQL_LENGTH) {
     if (!sql || !global.settings.logQueries) {
         return null;
     }
 
     if (typeof sql === 'string') {
         return {
-            simple: sql.substring(0, MAX_SQL_LENGTH)
+            type: sqlType,
+            sql: sql.substring(0, MAX_SQL_LENGTH)
         };
     }
 
     if (Array.isArray(sql)) {
         return {
-            multiple: sql.map(q => q.substring(0, MAX_SQL_LENGTH))
+            type: sqlType,
+            sql: sql.map(q => q.substring(0, MAX_SQL_LENGTH))
         };
     }
 
+    // other cases from Batch API
     return {
-        other: sql
+        type: sqlType,
+        sql: sql
     };
 }
