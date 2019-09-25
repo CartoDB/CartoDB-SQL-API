967b79ac1ec26e715c3db1de752bfe5ca66a7c19
Román Jiménez <drrouman@gmail.com>
2019-09-25 17:24:13 +0200

    Fix line too long error

--- test/acceptance/app-configuration.js
+++ test/acceptance/app-configuration.js
@@ -5,6 +5,16 @@ require('../helper');
 var server = require('../../app/server')();
 var assert = require('../support/assert');
 
+const accessControlHeaders = [
+    'X-Requested-With',
+    'X-Prototype-Version',
+    'X-CSRF-Token',
+    'Authorization',
+    'Carto-Rate-Limit-Limit',
+    'Carto-Rate-Limit-Remaining',
+    'Carto-Rate-Limit-Reset',
+    'Retry-After'
+].join(', ');
 
 describe('app-configuration', function() {
 
@@ -61,7 +71,7 @@ describe('app-configuration', function() {
         }, RESPONSE_OK, function(err, res) {
             assert.equal(
                 res.headers['access-control-allow-headers'],
-                'X-Requested-With, X-Prototype-Version, X-CSRF-Token, Authorization, Carto-Rate-Limit-Limit, Carto-Rate-Limit-Remaining, Carto-Rate-Limit-Reset, Retry-After'
+                accessControlHeaders
             );
             assert.equal(res.headers['access-control-allow-origin'], '*');
             done();
@@ -78,7 +88,7 @@ describe('app-configuration', function() {
             assert.equal(res.body, '');
             assert.equal(
                 res.headers['access-control-allow-headers'],
-                'X-Requested-With, X-Prototype-Version, X-CSRF-Token, Authorization, Carto-Rate-Limit-Limit, Carto-Rate-Limit-Remaining, Carto-Rate-Limit-Reset, Retry-After'
+                accessControlHeaders
             );
             assert.equal(res.headers['access-control-allow-origin'], '*');
             done();
@@ -160,7 +170,7 @@ describe('app-configuration', function() {
             assert.equal(res.headers['access-control-allow-origin'], '*');
             assert.equal(
                 res.headers['access-control-allow-headers'],
-                'X-Requested-With, X-Prototype-Version, X-CSRF-Token, Authorization, Carto-Rate-Limit-Limit, Carto-Rate-Limit-Remaining, Carto-Rate-Limit-Reset, Retry-After'
+                accessControlHeaders
             );
             done();
         });
