158f28c8e748a42cddd12934c0acd014ad1a6bc5
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-10-01 11:11:19 +0200

    ES6 class and middlewares for version controller

--- app/controllers/version_controller.js
+++ app/controllers/version_controller.js
@@ -1,18 +1,17 @@
 'use strict';
 
-var version = {
-    cartodb_sql_api: require(__dirname + '/../../package.json').version
+const versions = {
+    cartodb_sql_api: require('./../../package.json').version
 };
 
-function VersionController() {
+module.exports = class VersionController {
+    route (app) {
+        app.get(`${global.settings.base_url}/version`, version());
+    }
+};
+
+function version () {
+    return function versionMiddleware (req, res) {
+        res.send(versions);
+    };
 }
-
-VersionController.prototype.route = function (app) {
-    app.get(global.settings.base_url + '/version', this.handleVersion.bind(this));
-};
-
-VersionController.prototype.handleVersion = function (req, res) {
-    res.send(version);
-};
-
-module.exports = VersionController;
