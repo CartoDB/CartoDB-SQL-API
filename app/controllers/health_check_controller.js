51dd9a79991db3769b6cf5da26fe20cd2ca4f26a
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-10-01 10:52:31 +0200

    ES6 class and middlewares for healtch-check controller

--- app/controllers/health_check_controller.js
+++ app/controllers/health_check_controller.js
@@ -1,35 +1,40 @@
 'use strict';
 
-var HealthCheck = require('../monitoring/health_check');
+const HealthCheckBackend = require('../monitoring/health_check');
 
-function HealthCheckController() {
-    this.healthCheck = new HealthCheck(global.settings.disabled_file);
-}
+module.exports = class HealthCheckController {
+    constructor () {
+        this.healthCheckBackend = new HealthCheckBackend(global.settings.disabled_file);
+    }
 
-HealthCheckController.prototype.route = function (app) {
-    app.get(global.settings.base_url + '/health', this.handleHealthCheck.bind(this));
-};
-
-HealthCheckController.prototype.handleHealthCheck = function (req, res) {
-    var healthConfig = global.settings.health || {};
-    if (!!healthConfig.enabled) {
-        var startTime = Date.now();
-        this.healthCheck.check(function(err) {
-            var ok = !err;
-            var response = {
-                enabled: true,
-                ok: ok,
-                elapsed: Date.now() - startTime
-            };
-            if (err) {
-                response.err = err.message;
-            }
-            res.status(ok ? 200 : 503).send(response);
-
-        });
-    } else {
-        res.status(200).send({enabled: false, ok: true});
+    route (app) {
+        app.get(`${global.settings.base_url}/health`, healthCheck({ healthCheckBackend: this.healthCheckBackend }));
     }
 };
 
-module.exports = HealthCheckController;
+function healthCheck ({ healthCheckBackend }) {
+    return function healthCheckMiddleware (req, res) {
+        const healthConfig = global.settings.health || {};
+
+        if (!healthConfig.enabled) {
+            return res.status(200).send({enabled: false, ok: true});
+        }
+
+        const startTime = Date.now();
+
+        healthCheckBackend.check((err) => {
+            const ok = !err;
+            const response = {
+                enabled: true,
+                ok,
+                elapsed: Date.now() - startTime
+            };
+
+            if (err) {
+                response.err = err.message;
+            }
+
+            res.status(ok ? 200 : 503).send(response);
+        });
+    };
+}
