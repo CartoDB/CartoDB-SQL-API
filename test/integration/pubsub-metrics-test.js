58032ddc37acebf56102a0887487c3ff5b849300
Esther Lozano <esloho@gmail.com>
2020-02-19 17:49:53 +0100

    Refactor service to ease integration test

--- test/integration/pubsub-metrics-test.js
+++ test/integration/pubsub-metrics-test.js
@@ -19,59 +19,68 @@ var request = {
     method: 'GET'
 };
 
+const fakeTopic = {
+    name: 'test-topic',
+    publish: sinon.stub().returns(Promise.resolve())
+};
+
+const fakePubSub = {
+    topic: () => fakeTopic
+};
+
 describe('pubsub metrics middleware', function () {
     let server;
-    let isDisabledStub;
-    let sendStub;
     let clock;
 
     before(function () {
         clock = sinon.useFakeTimers();
-        isDisabledStub = sinon.stub(PubSubMetricsService.prototype, 'isDisabled');
-        sendStub = sinon.stub(PubSubMetricsService.prototype, 'sendEvent');
-        server = app();
+        sinon.stub(PubSubMetricsService, 'createPubSub').returns(fakePubSub);
     });
 
     after(function () {
         clock.restore();
-        PubSubMetricsService.prototype.isDisabled.restore();
-        PubSubMetricsService.prototype.sendEvent.restore();
+        PubSubMetricsService.createPubSub.restore();
+        global.settings.pubSubMetrics.enabled = false;
     });
 
     it('should not send event if disabled', function (done) {
-        isDisabledStub.returns(true);
+        global.settings.pubSubMetrics.enabled = false;
+        server = app();
 
         assert.response(server, request, { status: 200 }, function (err, res) {
             if (err) {
                 return done(err);
             }
 
-            assert(sendStub.notCalled);
-            done();
+            assert(fakeTopic.publish.notCalled);
+            return done();
         });
     });
 
     it('should not send event if headers not present', function (done) {
-        isDisabledStub.returns(false);
+        global.settings.pubSubMetrics.enabled = true;
+        server = app();
 
         assert.response(server, request, { status: 200 }, function (err, res) {
             if (err) {
                 return done(err);
             }
 
-            assert(sendStub.notCalled);
-            done();
+            assert(fakeTopic.publish.notCalled);
+            return done();
         });
     });
 
     it('should send event if headers present', function (done) {
-        isDisabledStub.returns(false);
+        global.settings.pubSubMetrics.enabled = true;
+        server = app();
         request.headers['Carto-Event'] = 'test-event';
         request.headers['Carto-Event-Source'] = 'test';
         request.headers['Carto-Event-Group-Id'] = '1';
 
         const eventAttributes = {
             event_source: 'test',
+            user_id: '1',
             event_group_id: '1',
             response_code: '200',
             source_domain: request.headers.host,
@@ -84,8 +93,8 @@ describe('pubsub metrics middleware', function () {
                 return done(err);
             }
 
-            assert(sendStub.calledOnceWith('vizzuality', 'test-event', eventAttributes, sinon.match.any));
-            done();
+            assert(fakeTopic.publish.calledOnceWith(Buffer.from('test-event'), eventAttributes));
+            return done();
         });
     });
 });
--- test/unit/pubsub-metrics-test.js
+++ test/unit/pubsub-metrics-test.js
@@ -3,84 +3,40 @@
 require('../helper');
 
 const sinon = require('sinon');
-
-const MetadataDB = require('cartodb-redis');
 const assert = require('../support/assert');
 const PubSubMetricsService = require('../../lib/services/pubsub-metrics');
 
-const metadataBackend = new MetadataDB({
-    host: global.settings.redis_host,
-    port: global.settings.redis_port,
-    max: global.settings.redisPool,
-    idleTimeoutMillis: global.settings.redisIdleTimeoutMillis,
-    reapIntervalMillis: global.settings.redisReapIntervalMillis
-});
+const fakeTopic = {
+    name: 'test-topic',
+    publish: sinon.stub().returns(Promise.resolve())
+};
+
+const fakePubSub = {
+    topic: () => fakeTopic
+};
+
+const eventAttributes = {
+    event_source: 'test',
+    user_id: '123',
+    event_group_id: '1',
+    response_code: '200',
+    source_domain: 'vizzuality.cartodb.com',
+    event_time: new Date().toISOString(),
+    event_version: '1'
+};
 
 describe('pubsub metrics service', function () {
-    let isDisabledStub;
-    let publishStub;
+    it('should not send event not enabled', function () {
+        const pubSubMetricsService = new PubSubMetricsService(fakePubSub, false);
 
-    before(function () {
-        global.settings.pubSubMetrics.topic = 'test-topic';
-
-        sinon.stub(PubSubMetricsService.prototype, 'initPubSub');
-        sinon.stub(PubSubMetricsService.prototype, 'getTopic');
-        publishStub = sinon.stub(PubSubMetricsService.prototype, 'publish');
-        isDisabledStub = sinon.stub(PubSubMetricsService.prototype, 'isDisabled');
+        pubSubMetricsService.sendEvent('test-event', eventAttributes);
+        assert(fakeTopic.publish.notCalled);
     });
 
-    after(function () {
-        global.settings.pubSubMetrics.topic = '';
-        PubSubMetricsService.prototype.initPubSub.restore();
-        PubSubMetricsService.prototype.getTopic.restore();
-        PubSubMetricsService.prototype.isDisabled.restore();
-        PubSubMetricsService.prototype.publish.restore();
-    });
+    it('should send event if enabled', function () {
+        const pubSubMetricsService = new PubSubMetricsService(fakePubSub, true);
 
-    it('should not send event if not enabled', function (done) {
-        isDisabledStub.returns(true);
-        const pubSubMetricsService = new PubSubMetricsService(metadataBackend);
-        const username = 'vizzuality';
-        const event = 'test-event';
-        const attributes = {
-            event_source: 'test',
-            event_group_id: '1',
-            response_code: '200',
-            source_domain: 'vizzuality.cartodb.com',
-            event_version: '1'
-        };
-
-        pubSubMetricsService.sendEvent(username, event, attributes, function (err, res) {
-            if (err) {
-                return done(err);
-            }
-
-            assert(publishStub.notCalled);
-            done();
-        });
-    });
-
-    it('should send event with user_id if enabled', function (done) {
-        isDisabledStub.returns(false);
-        const pubSubMetricsService = new PubSubMetricsService(metadataBackend);
-        const username = 'vizzuality';
-        const event = 'test-event';
-        const attributes = {
-            event_source: 'test',
-            event_group_id: '1',
-            response_code: '200',
-            source_domain: 'vizzuality.cartodb.com',
-            event_version: '1'
-        };
-
-        const expectedAttributes = Object.assign({ user_id: '1' }, attributes);
-        pubSubMetricsService.sendEvent(username, event, attributes, function (err, res) {
-            if (err) {
-                return done(err);
-            }
-
-            assert(publishStub.calledOnceWith(event, expectedAttributes));
-            done();
-        });
+        pubSubMetricsService.sendEvent('test-event', eventAttributes);
+        assert(fakeTopic.publish.calledOnceWith(Buffer.from('test-event'), eventAttributes));
     });
 });
